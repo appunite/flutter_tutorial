37cda0cfdf079747e52f2efcc49e46bb9be25759
Michał Nowak <michal.nowak@appunite.com>
2021-06-30 20:28:29 +0200

    add factory constructors to tutorial entry

--- lib/tutorial_entry.dart
+++ lib/tutorial_entry.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/get_basic_rrect.dart';
 
 class TutorialEntry {
   const TutorialEntry(this.rrectList);
 
-  /// Object references to highlight
+  TutorialEntry.single(RRect rrect) : rrectList = [rrect];
+
+  TutorialEntry.fromKey(GlobalKey key)
+      : rrectList = [
+          getBasicRRect(key),
+        ];
+
+  TutorialEntry.multipleKeys(List<GlobalKey> keys)
+      : rrectList = keys
+            .map(
+              (key) => getBasicRRect(key),
+            )
+            .toList();
+
+  /// Object references to highlight.
   final List<RRect> rrectList;
 }
